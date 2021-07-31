e574f4a9157b4cc0e027e8291596a1a9a6a5c637
Tim Konieczny <hello@timkonieczny.com>
2021-07-31 13:28:57 +0100

    Added basic slider without rotation

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ import './style.css'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import * as dat from 'dat.gui'
 import gsap from 'gsap'
-import { AmbientLight, Group, Mesh, MeshBasicMaterial, PerspectiveCamera, PlaneBufferGeometry, Scene, WebGLRenderer } from "three"
+import { AmbientLight, Color, Group, Mesh, MeshBasicMaterial, PerspectiveCamera, PlaneBufferGeometry, Scene, WebGLRenderer } from "three"
 
 /**
  * Base
@@ -27,31 +27,43 @@ const light = new AmbientLight(0xffffff, 5.0)
 scene.add(light)
 
 const slider = new Group()
-const pictureMaterial = new MeshBasicMaterial()
 const pictureGeometry = new PlaneBufferGeometry()
 const numberOfPictures = 11
 for (let i = 0; i < 11; i++) {
+    const pictureMaterial = new MeshBasicMaterial({ color: new Color(Math.random(), Math.random(), Math.random()) })
     slider.add(new Mesh(pictureGeometry, pictureMaterial))
 }
 scene.add(slider)
 
-const previousPicture = _ => {
-    gsap.to(slider.position, { duration: 1, x: -1 })
-}
-const nextPicture = _ => {
-    gsap.to(slider.position, { duration: 1, x: 1 })
-}
-
-const rearrangePictures = activeIndex => {
-    const leftIndices = []
-    for (let i = 0; i < Math.floor(slider.children.length * .5); i++) {
-        leftIndices.push((activeIndex + i + 1) % numberOfPictures)
-    }
+const rearrangePictures = (activeIndex, withAnimation = true) => {
     const rightIndices = []
+    for (let i = 0; i < Math.floor(slider.children.length * .5); i++) {
+        rightIndices.push((activeIndex + i + 1) % numberOfPictures)
+    }
+    const leftIndices = []
     for (let i = 0; i < Math.floor(slider.children.length * .5); i++) {
         let index = activeIndex - i - 1
         index = index < 0 ? index + numberOfPictures : index
-        rightIndices.push(index)
+        leftIndices.push(index)
+    }
+    rightIndices.forEach((pictureIndex, i) => {
+        const child = slider.children[pictureIndex]
+        setPosition(child, 2, 0, -i - 1, withAnimation)
+    })
+    leftIndices.forEach((pictureIndex, i) => {
+        const child = slider.children[pictureIndex]
+        setPosition(child, -2, 0, -i - 1, withAnimation)
+    })
+
+    const activeChild = slider.children[activeIndex]
+    setPosition(activeChild, 0, 1, 0, withAnimation)
+}
+
+const setPosition = (mesh, x, y, z, withAnimation = true) => {
+    if (withAnimation) {
+        gsap.to(mesh.position, { duration: 1, x, y, z })
+    } else {
+        mesh.position.set(x, y, z)
     }
 }
 
@@ -62,14 +74,14 @@ previousButton.addEventListener("click", _ => {
     activePicture -= 1
     activePicture = activePicture < 0 ? activePicture + numberOfPictures : activePicture
     rearrangePictures(activePicture)
-    previousPicture()
 })
 nextButton.addEventListener("click", _ => {
     activePicture += 1
     activePicture %= numberOfPictures
     rearrangePictures(activePicture)
-    nextPicture()
 })
+
+rearrangePictures(activePicture, false)
 
 /**
  * Sizes
@@ -100,6 +112,7 @@ window.addEventListener('resize', () => {
 const camera = new PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.set(0, 0, 1)
 scene.add(camera)
+gui.add(camera.position, "x", -2, 2, 0.001).name("camera x")
 
 /**
  * Renderer

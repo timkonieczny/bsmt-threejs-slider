6aadf616a96165bb6ce05c2a31e6e12be6d4f868
Tim Konieczny <hello@timkonieczny.com>
2021-07-31 12:21:57 +0100

    Added tunnel and pictures

--- src/script.js
+++ src/script.js
@@ -1,32 +1,86 @@
 import './style.css'
-import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import * as dat from 'dat.gui'
+import { AmbientLight, Group, Mesh, MeshBasicMaterial, PerspectiveCamera, PlaneBufferGeometry, Scene, WebGLRenderer } from "three"
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI()
+
+const gltfLoader = new GLTFLoader()
+
+gltfLoader.load("/models/Tunnel.glb", gltf => {
+    console.log(gltf)
+    scene.add(gltf.scene)
+})
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new Scene()
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const light = new AmbientLight(0xffffff, 5.0)
+scene.add(light)
 
-// Sizes
+const slider = new Group()
+const pictureMaterial = new MeshBasicMaterial()
+const pictureGeometry = new PlaneBufferGeometry()
+for (let i = 0; i <= 10; i++) {
+    slider.add(new Mesh(pictureGeometry, pictureMaterial))
+}
+scene.add(slider)
+
+/**
+ * Sizes
+ */
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
 
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(0, 0, 1)
 scene.add(camera)
 
-// Renderer
-const renderer = new THREE.WebGLRenderer({
+/**
+ * Renderer
+ */
+const renderer = new WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const tick = () => {
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()

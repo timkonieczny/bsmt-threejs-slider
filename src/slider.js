1ca2c56ba3c9e12684f4b41aa8f64fff5e54e2a5
Tim Konieczny <hello@timkonieczny.com>
2021-09-02 12:39:00 +0100

    Added artworks

--- src/slider.js
+++ src/slider.js
@@ -1,15 +1,15 @@
 import gsap from 'gsap'
 import { Color, Euler, Group, Mesh, MeshBasicMaterial, PlaneBufferGeometry, Quaternion, SpotLight, sRGBEncoding, TextureLoader } from "three"
 import { CSS3DObject } from "three/examples/jsm/renderers/CSS3DRenderer"
-import image0 from '../assets/0.jpg'
 import image1 from '../assets/1.jpg'
+import image0 from '../assets/2.jpg'
 import image3 from '../assets/3.jpg'
 import image4 from '../assets/4.jpg'
 import image5 from '../assets/5.jpg'
 import image6 from '../assets/6.jpg'
 import image7 from '../assets/7.jpg'
-import image8 from '../assets/0.jpg'
-import image9 from '../assets/0.jpg'
+import image8 from '../assets/1.jpg'
+import image9 from '../assets/1.jpg'
 import * as Vibrant from 'node-vibrant'
 
 const ARTWORK_OFFSET_X = 5
